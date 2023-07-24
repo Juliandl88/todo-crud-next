9872439e60afd7b17693356820cd506f7a9a7e7e
Julian <julian_delisio@hotmail.com>
2023-07-23 23:17:19 -0300

    se agrega auth con firebase I

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAn2zJsnkL85TvyImRYvGB1P7kVb4QYcgE",
+  authDomain: "todo-crud-next.firebaseapp.com",
+  projectId: "todo-crud-next",
+  storageBucket: "todo-crud-next.appspot.com",
+  messagingSenderId: "1524743047",
+  appId: "1:1524743047:web:423a82f17b6992c4cb3122"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
